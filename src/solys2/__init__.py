5336433fbb18c3fb8529fb6dddc00e152b565298
Javier Gatón Herguedas <javigaton@gmail.com>
2022-04-11 13:52:25 +0200

    changed import order

--- src/solys2/__init__.py
+++ src/solys2/__init__.py
@@ -1,12 +1,12 @@
 try:
-    from . import solys2
-    from . import connection
-    from . import automation
     from . import response
+    from . import connection
     from . import common
+    from . import solys2
+    from . import automation
 except:
     from solys2 import connection
-    from solys2 import automation
     from solys2 import response
     from solys2 import common
+    from solys2 import automation
     from solys2 import solys2
