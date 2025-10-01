749ad8d99bb3ccf0ff34f98e9a3c22d7368e11a3
Javier Gatón Herguedas <javigaton@gmail.com>
2025-10-01 13:55:55 +0200

    version name to 0.2.9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ copyright = '2022, GOA-UVa (The Atmospheric Optics Group of Valladolid Universit
 author = 'GOA-UVa (The Atmospheric Optics Group of Valladolid University)'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.8'
+release = '0.2.9'
 
 
 # -- General configuration ---------------------------------------------------
